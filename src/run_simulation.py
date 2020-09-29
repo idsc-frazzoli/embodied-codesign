24f1bb77d8ade71bea4972860c3359070a6f81ce
Dejan Milojevic <milojevic.de@gmail.com>
2020-09-29 22:36:22 +0200

    Generating fn and fp curves with different sensors

--- src/run_simulation.py
+++ src/run_simulation.py
@@ -1,7 +1,10 @@
 from decimal import Decimal
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.interpolate import interp1d
 
 from controller.basic_controller import BasicController
-from sensing.sensing_performance import SensingParameters, SensingPerformance
+from sensing.sensing_performance import SensingParameters, SensingPerformance, get_recall_precision, CameraSpecification
 from simulator.simulator import simulate, SimParameters
 from vehicle.state_estimation import Prior
 from vehicle.vehicle import VehicleStats
@@ -18,6 +21,54 @@ if __name__ == '__main__':
     sens_param = SensingParameters(ds=ds, max_distance=max_distance, n=n,
                                    list_of_ds=list_of_ds, frequency=1*dt, latency= 1*dt)
     prior = Prior(density=Decimal('0.01'))
+    recall, precision = get_recall_precision(str("faster_rcnn96"), n, ds)
+    f_recall = interp1d(range(n), recall)
+    f_precision = interp1d(range(n), precision)
+    cam_basler = CameraSpecification(f=Decimal(str(4.0)), height=Decimal(str(4.55)), width=Decimal(str(6.17)),
+                                 n_pixel_height=1920, n_pixel_width=1200)
+    cam_flea = CameraSpecification(f=Decimal(str(4.0)), height=Decimal(str(6.2)), width=Decimal(str(7.6)),
+                                     n_pixel_height=1384, n_pixel_width=1032)
+
+    rfov_basler = cam_basler.get_vertical_fov(list_of_ds)
+    resolution_basler = cam_basler.get_vertical_resolution(rfov_basler)
+    recall_basler = [f_recall(float(resolution_basler[i])) for i in range(len(resolution_basler))]
+    precision_basler = [f_precision(float(resolution_basler[i])) for i in range(len(resolution_basler))]
+    fn_basler = 1 - np.array(recall_basler)
+    fp_basler = 1 - np.array(precision_basler)
+
+    rfov_flea = cam_flea.get_vertical_fov(list_of_ds)
+    resolution_flea = cam_flea.get_vertical_resolution(rfov_flea)
+    recall_flea = [f_recall(float(resolution_flea[i])) for i in range(len(resolution_flea))]
+    precision_flea = [f_precision(float(resolution_flea[i])) for i in range(len(resolution_flea))]
+    fn_flea = 1 - np.array(recall_flea)
+    fp_flea = 1 - np.array(precision_flea)
+
+    fig, ((ax1, ax2), (ax3, ax4), (ax5, ax6)) = plt.subplots(3, 2)
+    fig.suptitle('Recall, Precision, FPR, FNR dependent on distance/object height')
+    ax1.plot(range(len(recall)), recall)
+    ax1.set(xlabel='Object height in pixel', ylabel='Recall')
+    ax2.plot(range(len(precision)), precision)
+    ax2.set(xlabel='Object height in pixel', ylabel='Precision')
+    ax3.plot(list_of_ds, recall_basler, color="red", label="Basler")
+    ax3.plot(list_of_ds, recall_flea, color="blue", label="FLIR")
+    ax3.legend(loc="upper right")
+    ax3.set(xlabel='Distance in [m]', ylabel='Recall')
+    ax4.plot(list_of_ds, precision_basler, color="red", label="Basler")
+    ax4.plot(list_of_ds, precision_flea, color="blue", label="FLIR")
+    ax4.legend(loc="upper right")
+    ax4.set(xlabel='Distance in [m]', ylabel='Precision')
+    ax5.plot(list_of_ds, fn_basler, color="red", label="Basler")
+    ax5.plot(list_of_ds, fn_flea, color="blue", label="FLIR")
+    ax5.legend(loc="lower right")
+    ax5.set(xlabel='Distance in [m]', ylabel='FNR')
+    ax6.plot(list_of_ds, fp_basler, color="red", label="Basler")
+    ax6.plot(list_of_ds, fp_flea, color="blue", label="FLIR")
+    ax6.legend(loc="lower right")
+    ax6.set(xlabel='Distance in [m]', ylabel='FPR')
+    plt.savefig('Camera-Performance.png')
+    plt.show()
+
+
     fn = [Decimal(str((ds*i/(max_distance*2))**4)) for i in range(n)]
     fp = [Decimal(str((ds * i / (max_distance * 3/2)) ** 4)) for i in range(n)]
     lsd = [Decimal(str(ds * i/(max_distance) + Decimal(0.5))) for i in range(n)]
--- src/sensing/sensing_performance.py
+++ src/sensing/sensing_performance.py
@@ -1,6 +1,8 @@
+import numpy as np
 from dataclasses import dataclass
 from decimal import Decimal
-from typing import List
+from typing import List, Tuple
+from scipy.optimize import curve_fit
 
 
 @dataclass
@@ -49,3 +51,48 @@ class SensingPerformance:
         except IndexError:
             print("Index out of bound.")
 
+
+@dataclass
+class CameraSpecification:
+    f: Decimal  # focal length in [mm]
+    width: Decimal  # sensor chip width [mm]
+    height: Decimal  # sensor chip height [mm]
+    n_pixel_width: int  # width of image in pixels
+    n_pixel_height: int  # height of image in pixels
+
+    def __init__(self, f: Decimal, width: Decimal, height: Decimal, n_pixel_width: int, n_pixel_height: int):
+        self.f = f
+        self.width = width
+        self.height = height
+        self.n_pixel_height = n_pixel_height
+        self.n_pixel_width = n_pixel_width
+
+    def get_vertical_fov(self, list_of_ds: List[Decimal]) -> List[Decimal]:
+        vfov = self.height / self.f * np.array(list_of_ds)
+
+        return list(vfov)
+
+    def get_vertical_resolution(self, fov: List[Decimal]) -> List[Decimal]:
+        resolution_v = [Decimal(min(float(self.n_pixel_height), float(self.n_pixel_height/max(0.0001, float(fov[i])))))
+                        for i in range(len(fov))]
+
+        return resolution_v
+
+
+def func_recall_precision(x, a, b, c):
+    return a * x ** b + c
+
+
+def get_recall_precision(name_algo: str, n: int, ds: Decimal) -> Tuple[List[Decimal], List[Decimal]]:
+    if name_algo == "faster_rcnn96":
+        x_data = [0, 20, 50, 100, 150, 200, 250, 300, 350, 1032]
+        y_data_recall = [0, 0.875, 0.925, 0.96, 0.96, 0.96, 0.96, 0.97, 0.975, 0.999]
+        popt_recall, pcov_recall = curve_fit(func_recall_precision, x_data, y_data_recall)
+        recall = [Decimal(max(0, min(1, func_recall_precision(float(i), *popt_recall)))) for i in range(n)]
+        y_data_precision = [0, 0.7, 0.8, 0.92, 0.94, 0.955, 0.96, 0.97, 0.975, 0.999]
+        popt_precision, pcov_precision = curve_fit(func_recall_precision, x_data, y_data_precision)
+        precision = [Decimal(max(0, min(1, func_recall_precision(float(i), *popt_precision)))) for i in range(n)]
+    else:
+        raise ValueError("No or wrong algorithm name.")
+
+    return recall, precision
