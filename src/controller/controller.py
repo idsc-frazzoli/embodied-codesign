60386b29f7ca71793109cba2f753094b8ec58930
Dejan Milojevic <milojevic.de@gmail.com>
2020-10-16 13:29:12 +0200

    Testing packages, test basic controller example and some changes in basic controller.

--- src/controller/controller.py
+++ src/controller/controller.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from decimal import Decimal
 
 from vehicle.state_estimation import Belief, Action
 from vehicle.vehicle import VehicleState
@@ -9,3 +10,5 @@ class Controller(ABC):
     def get_action(self, vstate: VehicleState, belief: Belief) -> Action:
         ...
 
+    def get_critical_distance(self, v: Decimal) -> Decimal:
+        ...
